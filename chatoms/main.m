162b7905f2144f02adb7893690b520a3b87801fc
Gant <GantMan@gmail.com>
2011-11-13 09:47:41 -0600

    Initial Commit

---
+++ chatoms/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  chatoms
+//
+//  Created by Gant Laborde on 11/13/11.
+//  Copyright Gantix LLC 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
