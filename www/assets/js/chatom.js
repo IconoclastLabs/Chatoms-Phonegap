79571fd903f7a39af6a9e461163a849cf85124ca
Gant <GantMan@gmail.com>
2012-01-04 22:36:18 -0600

    Got categories working, it's ready for detaching from web

--- www/assets/js/chatom.js
+++ www/assets/js/chatom.js
@@ -1,17 +1,21 @@
 $(document).ready(function() {
+  
+	window.categories = [];
+		$('input:checkbox').each(function() {
+		return window.categories.push($(this).attr('name'));
+	});				  
+				  
+				  
   $("#getNewChatom").unbind('click').click(function(event) {
     event.preventDefault();
-    //alert("YO");
     return newChatom(); 
   });
 
-
-
   newChatom = function() 
   {
-    return $.getJSON('http://chatoms.com/chatom.json?personal=2', $(".categoryRadio:checked"), function(chatom) 
+    return $.getJSON('http://chatoms.com/chatom.json', $(".categoryRadio:checked"), function(chatom) 
     {
-        //$('.category-name').text(window.categories[chatom.starters_category_id - 1]);
+        $('.category-name').text(window.categories[chatom.starters_category_id - 1]);
         $("#curChatom").hide().text(chatom.text).fadeIn();
     }); // end getJSON
 
